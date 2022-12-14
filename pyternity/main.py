61e72256fb767aa04b78f5a77d12fb6607b8cdd9
cpAdm <57954026+cpAdm@users.noreply.github.com>
2022-12-14 11:50:58 +0100

    Run on all popular projects as default

--- pyternity/main.py
+++ pyternity/main.py
@@ -1,8 +1,6 @@
-from operator import attrgetter
-
 from pyternity.features import most_popular_per_version
 from pyternity.plotting import plot_signature
-from pyternity.pypi_crawler import PyPIProject
+from pyternity.pypi_crawler import PyPIProject, get_most_popular_projects
 from pyternity.utils import *
 
 
@@ -10,17 +8,27 @@ from pyternity.utils import *
 def main():
     setup_project()
 
-    projects = ("django",)
+    # TODO Add CLI (arguments) support
+    projects = get_most_popular_projects()
 
     for project_name in projects:
         project = PyPIProject(project_name)
 
-        for release in filter(attrgetter("is_major"), project.releases):
-            features = release.get_features()
-            print(most_popular_per_version(features))
+        releases = [release for release in project.releases if release.is_major]
+        for release in releases:
+            try:
+                features = release.get_features()
+            except RecursionError:
+                # Python files of pybullet cause this error; skip it
+                logger.warning(f"Maximum recursion depth exceeded for {release.project_name} {release.version}")
+                continue
 
             signature = {version: sum(features.values()) for version, features in features.items()}
             plot_signature(signature, release)
+
+        if not releases:
+            logger.warning(f"No major versions found for {project.name:30}: "
+                           f"{[release.version for release in project.releases]}")
 
 
 if __name__ == '__main__':
